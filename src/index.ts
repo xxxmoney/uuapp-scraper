60864fc0d33a337aca4dd882be37fb1753c68301
xxxmoney <xxxmoney111@gmail.com>
2025-04-05 13:27:41 +0200

    Proper loading of env

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,15 @@ if (envConfig.error) {
     throw new Error(`Failed to load configuration: ${envConfig.error}`);
 }
 
+console.log(`Loaded configuration: ${JSON.stringify(envConfig.parsed)}`);
+
 // Parse with schema
-const parsedEnvConfig = EnvSchema.parse(process.env);
+const parsedEnvConfig = EnvSchema.parse(envConfig.parsed);
 
 console.log(`Parsed configuration: ${JSON.stringify(parsedEnvConfig)}`);
 
-const processor = new Processor(parsedEnvConfig);
-const contents = await processor.process();
-
-// Save the contents to a file
-await fs.writeFile(OUTPUT_FILE, contents.join('\n\n'), 'utf-8');
+// const processor = new Processor(parsedEnvConfig);
+// const contents = await processor.process();
+//
+// // Save the contents to a file
+// await fs.writeFile(OUTPUT_FILE, contents.join('\n\n'), 'utf-8');
--- src/schemas.ts
+++ src/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const EnvSchema = z.object({
-  IS_DEBUG: z.boolean().default(false),
+  IS_DEBUG: z.coerce.boolean().default(false),
   AUTH_USERNAME: z.string(),
   AUTH_PASSWORD: z.string(),
   URL: z.string().url(),
